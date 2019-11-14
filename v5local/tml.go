3ceb413bb23e974b603dcbb3bf13ec793822a9f0
cs <ch5468@sina.com>
2019-11-14 12:27:47 +0800

    20191114-2

--- v5local/tml.go
+++ v5local/tml.go
@@ -5560,12 +5560,12 @@ func dataMru(d []byte, ip *int64, tra byte, tmladdr int64, portlocal *int) (lstf
 			m = fmt.Sprintf("%02x", d[j])
 			if gopsu.String2Int32(m[:1], 10) == 5 {
 				svrmsg.WlstTml.WlstSluB900.SluitemPara.HasEnableAlarm = 1
-			} else if gopsu.String2Int32(m[:1], 2) == 10 {
+			} else if gopsu.String2Int32(m[:1], 10) == 10 {
 				svrmsg.WlstTml.WlstSluB900.SluitemPara.HasEnableAlarm = 0
 			}
 			if gopsu.String2Int32(m[1:2], 10) == 5 {
 				svrmsg.WlstTml.WlstSluB900.SluitemPara.IsRunning = 1
-			} else if gopsu.String2Int32(m[1:2], 2) == 10 {
+			} else if gopsu.String2Int32(m[1:2], 10) == 10 {
 				svrmsg.WlstTml.WlstSluB900.SluitemPara.IsRunning = 0
 			}
 			j++
